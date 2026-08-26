37bd3f455404a52b932eb0f2e2be5656c1543f01
mefische <mefische@42>
2026-08-26 10:51:24 +0100

    ex01 fixed, improved implementation

--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mefische <mefische@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/24 13:29:43 by mefische          #+#    #+#             */
-/*   Updated: 2026/08/03 15:11:45 by mefische         ###   ########.fr       */
+/*   Updated: 2026/08/26 10:50:37 by mefische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,53 @@
 
 int	main()
 {
+	std::cout << "*DEFAULT CONSTRUCTOR TEST*" << std::endl;
+	ClapTrap	defaultTrap;
+
+	std::cout << "\n*NAMED CONSTRUCTOR*" << std::endl;
 	ClapTrap	bob("bob");
 	ClapTrap	patrick("patrick");
-	ClapTrap	plankton(bob);
+	ClapTrap	plankton("plankton");
 
-	patrick = plankton;
+	std::cout << "\n*COPY TEST*" << std::endl;
+	ClapTrap	planktonB("plankton");
 
-	std::cout << bob.getName() << std::endl;
-	std::cout << &bob << std::endl;
+	std::cout << "\n*ASSIGNMENT OPERATOR TEST*" << std::endl;
+	planktonB = bob;
 
-	std::cout << patrick.getName() << std::endl;
-	std::cout << &patrick << std::endl;
+	std::cout << "\n--- Object Info ---" << std::endl;
+	std::cout << defaultTrap.getName() << " at " << &defaultTrap << std::endl;
+	std::cout << bob.getName() << " at " << &bob << std::endl;
+	std::cout << patrick.getName() << " at " << &patrick << std::endl;
+	std::cout << plankton.getName() << " at " << &plankton << std::endl;
+	std::cout << planktonB.getName() << " at " << &plankton << std::endl;
 
-	std::cout << plankton.getName() << std::endl;
-	std::cout << &plankton << std::endl;
-
+	std::cout << "\n*ACTION TESTS*" << std::endl;
+	std::cout << "-- Bob attacks --" << std::endl;
 	bob.attack("patrick");
-	patrick.takeDamage(5);
-	patrick.takeDamage(20);
 
+	std::cout << "\n-- Patrick takes damage --" << std::endl;
+	patrick.takeDamage(5);
+	std::cout << "Patrick's hit points: " << patrick.getHitPoints() << std::endl;
+	patrick.takeDamage(20);
+	std::cout << "Patrick's hit points: " << patrick.getHitPoints() << std::endl;
+
+	std::cout << "\n-- Patrick tries to act while dead --" << std::endl;
 	patrick.attack("bob");
 	patrick.beRepaired(5);
+
+	std::cout << "\n-- Bob repairs himself --" << std::endl;
+	bob.beRepaired(5);
+
+	std::cout << "\n-- Bob drains all energy --" << std::endl;
+	for (int i = 0; i < 9; i++)
+		bob.beRepaired(1);
+	std::cout << "Bob's energy: " << bob.getEnergyPoints() << std::endl;
+		
+	std::cout << "\n-- Bob tries to act with no energy --" << std::endl;
+	bob.attack("patrick");
+	bob.beRepaired(5);
+
+	std::cout << "\n*DESTRUCTORS*" << std::endl;
 	return 0;
 }
